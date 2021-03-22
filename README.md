122de5603d5e9efdc6c40b9542d58e2f9d8ac3c7
taichiH <azumade.30@gmail.com>
2021-03-22 14:46:33 +0900

    remove other than sensing launch

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# autoware_launcher
-launch files for autoware
+### setup
+```
+mkdir -p ~/livox_ws/src
+cd ~/livox_ws/src/
+git clone -b debug/livox git@github.com:tier4/autoware_launcher.iv.universe.git
+git clone git@github.com:Livox-SDK/livox_ros2_driver.git
+cd ..
+colcon build --symlink-install
+```
+
+### hardware settings
+Please prepare three livox horizon devises and set serial number to config files.
+
+There are front_left, front_center, front_right files in below directory.
+```
+livox_ws/src/autoware_launcher.iv.universe/sensing_launch/config/aip_x1/default
+```
+
+please replace cmdline_input_bd_code to real device's one.
+```
+/**:
+  ros__parameters:
+    cmdline_input_bd_code: '100000000000000'
+```
+
+
+
+### launch
+```
+source ~/livox_ws/install/setup.bash
+rviz2 -d ~/livox_ws/src/autoware_launcher.iv.universe/livox_debug.rviz
+ros2 launch sensing_launch sensing.launch.xml sensor_model:=aip_x1
+```
--- sensing_launch/launch/livox_horizon.launch.py
+++ sensing_launch/launch/livox_horizon.launch.py
@@ -23,36 +23,7 @@ from launch_ros.actions import ComposableNodeContainer, LoadComposableNodes
 from launch_ros.descriptions import ComposableNode
 from launch.substitutions import EnvironmentVariable
 
-
-def get_vehicle_info(context):
-    path = LaunchConfiguration('vehicle_param_file').perform(context)
-    with open(path, 'r') as f:
-        p = yaml.safe_load(f)['/**']['ros__parameters']
-    p['vehicle_length'] = p['front_overhang'] + p['wheel_base'] + p['rear_overhang']
-    p['vehicle_width'] = p['wheel_tread'] + p['left_overhang'] + p['right_overhang']
-    p['min_longitudinal_offset'] = -p['rear_overhang']
-    p['max_longitudinal_offset'] = p['front_overhang'] + p['wheel_base']
-    p['min_lateral_offset'] = -(p['wheel_tread'] / 2.0 + p['right_overhang'])
-    p['max_lateral_offset'] = p['wheel_tread'] / 2.0 + p['left_overhang']
-    p['min_height_offset'] = 0.0
-    p['max_height_offset'] = p['vehicle_height']
-    return p
-
-
-def get_vehicle_mirror_info(context):
-    path = LaunchConfiguration('vehicle_mirror_param_file').perform(context)
-    with open(path, 'r') as f:
-        p = yaml.safe_load(f)['/**']['ros__parameters']
-    return p
-
-
 def launch_setup(context, *args, **kwargs):
-
-    pkg = 'pointcloud_preprocessor'
-
-    vehicle_info = get_vehicle_info(context)
-    vehicle_mirror_info = get_vehicle_mirror_info(context)
-
     bd_code_param_path = LaunchConfiguration('bd_code_param_path').perform(context)
     with open(bd_code_param_path, 'r') as f:
         bd_code_param = yaml.safe_load(f)['/**']['ros__parameters']
@@ -79,52 +50,6 @@ def launch_setup(context, *args, **kwargs):
         ]
     )
 
-    # set self crop box filter as a component
-    cropbox_self_component = ComposableNode(
-        package=pkg,
-        plugin='pointcloud_preprocessor::CropBoxFilterComponent',
-        name='self_crop_box_filter',
-        remappings=[
-            ('input', 'livox/lidar'),
-            ('output', 'self_cropped/pointcloud'),
-        ],
-        parameters=[{
-            'input_frame': LaunchConfiguration('base_frame'),
-            'output_frame': LaunchConfiguration('base_frame'),
-            'min_x': vehicle_info['min_longitudinal_offset'],
-            'max_x': vehicle_info['max_longitudinal_offset'],
-            'min_y': vehicle_info['min_lateral_offset'],
-            'max_y': vehicle_info['max_lateral_offset'],
-            'min_z': vehicle_info['min_height_offset'],
-            'max_z': vehicle_info['max_height_offset'],
-            'negative': True,
-            'use_sim_time': EnvironmentVariable(name='AW_ROS2_USE_SIM_TIME', default_value='False'),
-        }]
-    )
-
-    # set mirror crop box filter as a component
-    cropbox_mirror_component = ComposableNode(
-        package=pkg,
-        plugin='pointcloud_preprocessor::CropBoxFilterComponent',
-        name='mirror_crop_box_filter',
-        remappings=[
-            ('input', 'self_cropped/pointcloud'),
-            ('output', 'mirror_cropped/pointcloud'),
-        ],
-        parameters=[{
-            'input_frame': LaunchConfiguration('base_frame'),
-            'output_frame': LaunchConfiguration('base_frame'),
-            'min_x': vehicle_mirror_info['min_longitudinal_offset'],
-            'max_x': vehicle_mirror_info['max_longitudinal_offset'],
-            'min_y': vehicle_mirror_info['min_lateral_offset'],
-            'max_y': vehicle_mirror_info['max_lateral_offset'],
-            'min_z': vehicle_mirror_info['min_height_offset'],
-            'max_z': vehicle_mirror_info['max_height_offset'],
-            'negative': True,
-            'use_sim_time': EnvironmentVariable(name='AW_ROS2_USE_SIM_TIME', default_value='False'),
-        }]
-    )
-
     # set container to run all required components in the same process
     container = ComposableNodeContainer(
         name='pointcloud_preprocessor_container',
@@ -132,8 +57,7 @@ def launch_setup(context, *args, **kwargs):
         package='rclcpp_components',
         executable='component_container',
         composable_node_descriptions=[
-            cropbox_self_component,
-            cropbox_mirror_component,
+            livox_driver_component
         ],
         output='screen',
         parameters=[{
@@ -141,13 +65,7 @@ def launch_setup(context, *args, **kwargs):
         }],
     )
 
-    loader = LoadComposableNodes(
-        composable_node_descriptions=[livox_driver_component],
-        target_container=container,
-        condition=launch.conditions.IfCondition(LaunchConfiguration('launch_driver')),
-    )
-
-    return [container, loader]
+    return [container]
 
 
 def generate_launch_description():
@@ -166,10 +84,7 @@ def generate_launch_description():
     add_launch_arg('user_config_path', os.path.join(get_package_share_directory(
         "livox_ros2_driver"), "config/livox_lidar_config.json"))
     add_launch_arg('bd_code_param_path')
-    add_launch_arg('launch_driver')
     add_launch_arg('base_frame', 'base_link')
     add_launch_arg('sensor_frame', 'livox_frame')
-    add_launch_arg('vehicle_param_file')
-    add_launch_arg('vehicle_mirror_param_file')
 
     return launch.LaunchDescription(launch_arguments + [OpaqueFunction(function=launch_setup)])
